7ece4c395b3162976062d89ec843b7cf312dad4b
faruq1009 <68148748+faruq1009@users.noreply.github.com>
2020-08-16 20:58:38 +0900

    Add files via upload

---
+++ py.py
@@ -0,0 +1,25 @@
+import discord
+import os
+ 
+
+access_token = os.environ["BOT_TOKEN"]
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+  await client.change_presence(status=discord.Status.online, activity=discord.Game("/인증 해주는중"))
+  print("! 훈#7470") 
+  print(client.user.name) 
+  print(client.user.id) 
+   
+@client.event
+async def on_message(message):
+ 
+  if message.content.startswith('/인증'): 
+    author = message.guild.get_member(int(message.author.id))
+    role = discord.utils.get(message.guild.roles, name="친구") 
+    await author.add_roles(role)
+    await message.channel.send('인증이 완료 되었습니다😄') 
+
+client.run(access_token)
